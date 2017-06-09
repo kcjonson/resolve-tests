a573bf959e486474f79da2faecc18e0ae46461b6
Kevin Jonson <kevin.jonson@redfin.com>
2017-06-09 11:15:58 -0700

    another example

--- Button/index.js
+++ Button/index.js
@@ -1,4 +1,4 @@
-// This file will be resolved to first
+// This file will be resolved to second
 
 module.exports = function() {
    console.log('hello from index.js')
--- Button/index.web.js
+++ Button/index.web.js
@@ -1,4 +1,4 @@
-// This file will be resolved to second
+// This file will be resolved to third
 
 module.exports = function() {
    console.log('hello from index.web.js')
